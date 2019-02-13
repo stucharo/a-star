9d05c24102ad2c99909f4dd64f39f0f70040e1a2
Stuart Roy <stuartcharlesroy@gmail.com>
2019-02-13 19:40:46 +0000

    removed old a-star algorithm

--- a_star.py
+++ a_star.py
@@ -1,129 +1,113 @@
-import heapq
-from math import sin, cos, asin, pi, atan2, atan, acos
-from copy import deepcopy
-import numpy as np
-from scipy.interpolate import griddata
 from queue import PriorityQueue
-import matplotlib.pyplot as plt
-import random
-from heuristic import Point, copy_pt, dist, get_shortest_path, bend_point, direction
+from math import pi
 
-spacing = 1
-min_straight_length = 10
-bend_radius = [5]
-theta_tol = 3 * pi / 180
+import numpy as np
+
+from heuristic import Point, bend_point
+
+bend_rads = [5, 10]
+min_straight = 10
 heading_tol = pi/180
 location_tol = 1
-
-def get_neighbours(pt, goal):
-    ns = []
-    if pt.radius != 0:
-        # if we are on a bend and approaching the intersection with the last straight, we need to make sure that the bend neighbour doesn't continue further around the bend than neccessary and leave us unable to get to the end vector
-        # we keep on this bend or move the minimum straight length
-        bp = bend_point(pt, spacing, pt.radius)
-        bp.previous = pt
-        bp.cost = bp.previous.cost + cost(spacing)
-        ns.append(bp)
-        sp = copy_pt(pt, min_straight_length*pt.dx, min_straight_length*pt.dy, 0)
-        sp.radius = 0
-        sp.previous = pt
-        sp.cost = sp.previous.cost + cost(min_straight_length)
-        ns.append(sp)
-    else:
-        # we can try every bend angle
-        for r in bend_radius:
-            for d in [1, -1]:
-                dr = d * r
-                bp = bend_point(pt, spacing, dr)
-                bp.previous = pt
-                bp.cost = bp.previous.cost + cost(spacing)
-                ns.append(bp)
-        sp = copy_pt(pt, spacing*pt.dx, spacing*pt.dy, 0)
-        sp.previous = pt
-        sp.cost = sp.previous.cost + cost(spacing)
-        ns.append(sp)
-    return ns
+spacing = 1
 
 def cost(l):
     return l
 
-def a_star_pipe(start, goal):
-    path_ends = PriorityQueue()
-    start.cost = 0
-    path_ends.put((0, 0, start))
-    counter = 1
-    max_priority = 0
-
-    while not path_ends.empty():
-        # Get path end most likely to be cheapest
-        cur_point = path_ends.get()[2]
-
-        neighbours = get_neighbours(cur_point, goal)
-
-        for neighbour in neighbours:
-            
-            if dist(neighbour, goal) < location_tol and abs(neighbour.heading - goal.heading) < heading_tol:
-                return neighbour
-
-            hp = neighbour.get_heuristic_path(goal, min(bend_radius),
-                min_straight_length, min_straight_length, heading_tol,
-                location_tol, spacing)
-            
-            #graph(neighbour, goal, hp)
-            if len(hp) == 0:
-                hc = 100_000_000
-            else:
-                graph(neighbour, goal, hp)
-                hc = cost(len(hp) * spacing)
-
-            priority = neighbour.cost + hc
-
-            print(f"{counter}: {100*neighbour.cost/priority:.2f}% route solved")
-
-            path_ends.put((priority, counter, neighbour))
-
-            counter += 1
-
-    return end_point
-
-def graph(s, g, path):
-
-    c = s
-    prev_path = []
-    while c.previous is not None:
-        prev_path.append(c.previous)
-        c = c.previous
-    prev_path.reverse()
-    prev_path.extend(path)
-    if len(path) > 1:
-        ps = np.array([(p.x, p.y) for p in prev_path])
-        plt.plot(ps[:,0], ps[:,1])
-        plt.axis('equal')
-        plt.title(f"Cost: {cost(len(ps) * spacing)}")
+def get_alt_points(cp):
+    # this returns all path options from the current
+    # point EXCEPT the next point on the heuristic path
+    aps = []
+    # if cp is on a bend
+    if cp.radius != 0:
+        # we eith continue around the bend OR move straight
+        if cp.heuristic_path[0].radius == 0:
+            # return the bend point
+            bp = bend_point(cp, spacing, cp.radius)
+            bp.previous = cp
+            bp.cost = bp.previous.cost + cost(spacing)
+            aps.append(bp)
+        else:
+            #return the straight point the minimum straight distance away
+            sp = copy_pt(cp, min_straight*pt.dx, min_straight*pt.dy, 0)
+            sp.radius = 0
+            sp.previous = cp
+            sp.cost = sp.previous.cost + cost(min_straight)
+            aps.append(sp)
     else:
-        cx = (s.x+g.x)/2
-        cy = (s.y+g.y)/2
-        w = max(abs(g.x-s.x), abs(g.y-s.y))
-        plt.axis([cx-w/2-10,cx+w/2+10,cy-w/2-10,cy+w/2+10])
-    plt.arrow(s.x, s.y, 5*s.dx, 5*s.dy, head_width=0.5, color='green')
-    plt.arrow(g.x, g.y, 5*g.dx, 5*g.dy, head_width=0.5, color='red')
-    plt.show()
+        # we return all options except the heuristic option
+        # first, lets make a list of all the bend options
+        neg_rads = [-r for r in bend_rads]
+        rads = [r for r in bend_rads]
+        rads.extend(neg_rads)
+        # lets check if the heuristic path moves into a bend
+        if cp.heuristic_path[0].radius != 0:
+            # remove that radius
+            rads = list(filter(lambda x: x != cp.heuristic_path[0].radius), bend_rads)
+            # add a straight point the a unit distance away
+            sp = copy_pt(cp, spacing*cp.dx, spacing*cp.dy, 0)
+            sp.radius = 0
+            sp.previous = cp
+            sp.cost = sp.previous.cost + cost(spacing)
+            aps.append(sp)
+        # then add all the bend points
+        for r in rads:
+            bp = bend_point(cp, spacing, r)
+            bp.previous = cp
+            bp.cost = bp.previous.cost + cost(spacing)
+            aps.append(bp)
+    return aps
 
-if __name__ == '__main__':
+def a_star(s, g):
 
-    start = Point(10, 10, 0)
-    goal = Point(40, 40, pi/2)
+    paths = PriorityQueue()
+    counter = 0
+    route_cost = np.inf
+    last_tile = None
+    s.get_heuristic_path(g, min(bend_rads), min_straight, min_straight, heading_tol, location_tol, spacing)
+    paths.put((s.cost + cost(len(s.heuristic_path) * spacing), counter, s))
 
-    pt = a_star_pipe(start, goal)
-    fig, ax = plt.subplots()
-    ax.arrow(start.x, start.y, 5*start.dx, 5*start.dy, head_width=0.5, head_length=1)
-    ax.arrow(goal.x, goal.y, 5*goal.dx, 5*goal.dy, head_width=0.5, head_length=1)
-    pts = [(pt.x, pt.y)]
-    while pt.previous is not None:
-        pts.append((pt.previous.x, pt.previous.y))
-        pt = pt.previous
-    pts = np.array(pts)
-    plt.plot(pts[:,0], pts[:,1])
-    plt.axis([0,100,0,100], option='equal')
-    plt.show()
+    while not paths.empty():
 
+        # get the priority path
+        priority = paths.get()
+        current_point = priority[2]
+        print(f"Current point: {current_point}")
+        # move to the next point along the heuristic path
+        next_point = current_point.heuristic_path[0]
+        next_point.previous = current_point
+        next_point.cost = current_point.cost + cost(spacing)
+        if len(current_point.heuristic_path) == 1:
+            # the next point is the goal so there's no 
+            # point in exploring any options off this path
+            route_cost = next_point.cost
+            last_tile = next_point
+            print(last_tile)
+            continue
+        else:
+            next_point.heuristic_path = current_point.heuristic_path[1:]
+        # and push that back onto the queue with the same priority
+        counter += 1
+        paths.put((priority[0], counter, next_point))
+
+        # now get alternative path options from this point
+        alt_points = get_alt_points(current_point)
+        for p in alt_points:
+            p.get_heuristic_path(g, min(bend_rads), min_straight, min_straight, heading_tol, location_tol, spacing)
+            if len(p.heuristic_path) > 0:
+                priority = p.cost+cost(len(p.heuristic_path)*spacing)
+                if priority < route_cost:
+                    counter += 1
+                    paths.put((p.cost+cost(len(p.heuristic_path)*spacing),
+                               counter, p))
+    
+    return route_cost, last_tile
+
+start = Point(10, 10, 0)
+goal = Point(40, 40, pi/2)
+
+pt = a_star(start, goal)
+print(pt)
+
+
+        
